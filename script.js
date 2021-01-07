4b079d3682e715d32d5a3a57e73a6023e4ae412d
Alessandra Canuto <ale.canutto@gmail.com>
2021-01-07 12:48:22 -0300

    projeto modificado

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const dino = document.querySelector('.dino');
+const score = document.querySelector('.score');
 const background = document.querySelector('.background');
 
 let isJumping = false;
@@ -7,17 +8,48 @@ let position = 0;
 let count = 0;
 
 function handleKeyUp(event) {
-    if (event.keyCode === 32 && !isJumping) jump();
+    if (event.keyCode === 32) {
+
+        if (!isJumping) jump();
+
+        //inicia jogo
+        if (count === 0) {
+            background.style.backgroundRepeat = "repeat-x";
+            background.style.animation = "slideRight 600s infinite linear"
+            createCactus();
+
+            var xCount = setInterval(() => {
+
+                if (isGameOver) {
+                    clearInterval(xCount);                    
+                    return;
+                }
+
+                count++;
+                score.innerHTML = "Pontuação: " +  ("0000" + count).slice(-4);
+
+                if (dino.style.backgroundImage.toString().indexOf("images/t-rex3.png") > -1) {
+                    dino.style.backgroundImage = "url('./images/t-rex4.png')";
+                } else {
+                    dino.style.backgroundImage = "url('./images/t-rex3.png')";
+                }
+
+            }, 300);
+
+        }
+    }
 }
 
 function jump() {
+    if (isGameOver) return
     isJumping = true;
+    dino.style.backgroundImage = "url('./images/t-rex1.png')";
 
     let upInterval = setInterval(() => {
         if (position >= 150) {
             //descendo
             clearInterval(upInterval);
-            
+
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval);
@@ -37,24 +69,32 @@ function jump() {
 
 function createCactus() {
     const cactus = document.createElement('div');
-    let cactusPosition = 1000;
-    let randomTime = Math.random() * 6000;
+    let cactusPosition = 1200;
+    let randomTime = Math.random() * 6500;
+
+    if (isGameOver) return;
 
     cactus.classList.add('cactus');
     background.appendChild(cactus);
     cactus.style.left = cactusPosition + 'px';
-    
+
     let leftInterval = setInterval(() => {
-        if (cactusPosition < -60) {
+        if (cactusPosition < 0) {
             //saiu da tela
             clearInterval(leftInterval);
             background.removeChild(cactus);
-        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60 && !isJumping) {
             //game over
             isGameOver = true;
             clearInterval(leftInterval);
-            document.body.innerHTML = '<h1 class="game-over">Fim de jogo<h1>';
+            dino.style.backgroundImage = "url('./images/t-rex5.png')";
+            background.style.backgroundRepeat = "none";
+            background.style.animation = "none";
+            const info = document.createElement('div');
+            info.innerHTML = '<h1 class="game-over">Fim de jogo<h1>';
+            document.body.appendChild(info);            
         } else {
+            //move cactus
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
         }
@@ -64,6 +104,6 @@ function createCactus() {
 
 }
 
-createCactus();
+
 document.addEventListener('keyup', (handleKeyUp));
 
